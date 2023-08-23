eb680bc5aae677d2a93d4b9d7ee961eb509a1add
Vinicius Melo <vstmelo@proton.me>
2023-08-23 12:02:50 -0300

    fix: svg import

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+
   data = {
     id: 1,
     cliente: 'João',
     valor: '53.000',
   };
-  otherHtml = 'teste';
+
 }
