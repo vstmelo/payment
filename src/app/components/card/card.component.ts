ac6056ea3cc4ea0611c643fbd10466339b8ffbb7
Vinicius Melo <vstmelo@proton.me>
2023-09-04 12:46:07 -0300

    feat: edit card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -13,6 +13,10 @@ interface IData {
 })
 export class CardComponent implements OnInit {
   @Input() header: string | undefined;
+  @Input() field1: string | undefined;
+  @Input() field2: string | undefined;
+  @Input() field3: string | undefined;
+
   data!: IData[];
 
   constructor(private dataService: DataService) {}
